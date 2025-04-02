0ba3ee3c1bc50e6bcea8b643dc3fe2e13e2c3de2
Scill <realscill@gmail.com>
2025-04-02 19:51:46 -0400

    Add tp to eye location

--- src/main/java/me/colingrimes/root/command/teleport/Teleport.java
+++ src/main/java/me/colingrimes/root/command/teleport/Teleport.java
@@ -7,8 +7,11 @@ import me.colingrimes.midnight.command.handler.util.Sender;
 import me.colingrimes.midnight.util.bukkit.Worlds;
 import me.colingrimes.midnight.util.text.Text;
 import me.colingrimes.root.Root;
+import org.bukkit.FluidCollisionMode;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.util.RayTraceResult;
+import org.bukkit.util.Vector;
 
 import javax.annotation.Nonnull;
 import java.util.Optional;
@@ -17,7 +20,16 @@ public class Teleport implements Command<Root> {
 
 	@Override
 	public void execute(@Nonnull Root plugin, @Nonnull Sender sender, @Nonnull ArgumentList args) {
-		if (args.size() == 2) {
+		if (args.size() <= 1) {
+			Location eye = sender.player().getEyeLocation();
+			RayTraceResult result = sender.world().rayTrace(eye, eye.getDirection(), 200, FluidCollisionMode.NEVER, true, 0, e -> !e.equals(sender.player()));
+			if (result != null) {
+				Vector hit = result.getHitPosition();
+				sender.player().teleport(new Location(sender.world(), hit.getX(), hit.getY(), hit.getZ(), sender.location().getYaw(), sender.location().getPitch()));
+			} else {
+				sender.message(Text.color("&cThere is nowhere to teleport to."));
+			}
+		} else if (args.size() == 2) {
 			double x = args.getDoubleOrDefault(0, sender.x());
 			double z = args.getDoubleOrDefault(1, sender.z());
 			sender.player().teleport(new Location(sender.world(), x, sender.y(), z, sender.location().getYaw(), sender.location().getPitch()));
@@ -45,7 +57,6 @@ public class Teleport implements Command<Root> {
 	@Override
 	public void configureProperties(@Nonnull CommandProperties properties) {
 		properties.setAliases("tp");
-		properties.setArgumentsRequired(2);
 		properties.setPlayerRequired(true);
 	}
 }
