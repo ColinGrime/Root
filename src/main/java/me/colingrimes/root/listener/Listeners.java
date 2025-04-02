cfe97e738cfab495fc2296bcdcef990aa464b99d
Scill <realscill@gmail.com>
2025-04-02 18:39:47 -0400

    Add quick respawn functionality

--- src/main/java/me/colingrimes/root/listener/Listeners.java
+++ src/main/java/me/colingrimes/root/listener/Listeners.java
@@ -1,8 +1,11 @@
 package me.colingrimes.root.listener;
 
 import me.colingrimes.midnight.util.text.Text;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 import javax.annotation.Nonnull;
@@ -12,5 +15,19 @@ public class Listeners implements Listener {
 	@EventHandler
 	public void onAsyncPlayerChat(@Nonnull AsyncPlayerChatEvent event) {
 		event.setMessage(Text.color(event.getMessage()));
+	}
+
+	@EventHandler
+	public void onEntityDamage(@Nonnull EntityDamageEvent event) {
+		if (!(event.getEntity() instanceof Player player) || event.getDamage() < player.getHealth()) {
+			return;
+		}
+
+		event.setCancelled(true);
+		player.setFireTicks(0);
+		player.setFoodLevel(20);
+		player.setSaturation(20);
+		player.setHealth(player.getAttribute(Attribute.MAX_HEALTH).getValue());
+		player.sendMessage(Text.color("&cQuick respawn."));
 	}
 }
