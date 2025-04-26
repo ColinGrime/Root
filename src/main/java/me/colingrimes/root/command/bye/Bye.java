afaf8f48efc2cc5a374541d0677f96d1da348654
Scill <realscill@gmail.com>
2025-04-26 12:43:35 -0400

    Make /bye command remove all entities

--- src/main/java/me/colingrimes/root/command/bye/Bye.java
+++ src/main/java/me/colingrimes/root/command/bye/Bye.java
@@ -3,12 +3,9 @@ package me.colingrimes.root.command.bye;
 import me.colingrimes.midnight.command.Command;
 import me.colingrimes.midnight.command.handler.util.ArgumentList;
 import me.colingrimes.midnight.command.handler.util.Sender;
-import me.colingrimes.midnight.util.bukkit.Entities;
 import me.colingrimes.midnight.util.text.Text;
 import me.colingrimes.root.Root;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 
 import javax.annotation.Nonnull;
@@ -18,7 +15,7 @@ public class Bye implements Command<Root> {
 	@Override
 	public void execute(@Nonnull Root plugin, @Nonnull Sender sender, @Nonnull ArgumentList args) {
 		for (Entity entity : sender.world().getEntities()) {
-			if (entity instanceof LivingEntity && !(entity instanceof Player)) {
+			if (!(entity instanceof Player)) {
 				entity.remove();
 			}
 		}
